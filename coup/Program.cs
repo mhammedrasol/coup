7cd6ffe65d7d5e470eb5731793c5b2da1c52fe3d
mhammedrasol <83mbamad@gmail.com>
2024-10-26 15:23:30 +0300

    Add project files.

---
+++ coup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coup
+{
+    public class Vachal
+    {
+        public string brand;
+    }
+    public class motorvachal : Vachal
+    {
+        public string name;
+    }
+          
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                motorvachal car = new motorvachal();
+                car.name = "marseds";
+                car.brand = "c600";
+                motorvachal track = new motorvachal();
+                track.name = "volvo";
+                track.brand = "c100";
+                Console.WriteLine(car.name);
+                Console.WriteLine(car.brand);
+                Console.WriteLine(track.name);
+                Console.WriteLine(track.brand);
+                Console.ReadLine();
+
+            }
+        }
+}
